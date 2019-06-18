85d69cf9afe6efa71f0bf98671067e11dbaaba3a
Patrick Galliani <pjgalliani@caltech.edu>
2019-06-17 23:47:53 -0700

    Added types to src/deposit.py

--- src/deposit.py
+++ src/deposit.py
@@ -2,80 +2,89 @@
 # Hanna
 # src/deposit.py
 
+from src.asset_class import AssetClass
+from src.purchase import Purchase
 from src.util import dollar_str
 
 from prettytable import PrettyTable
+from typing import Any, Dict, List, KeysView, Tuple
 
 import json
 
 
 class Deposit:
     def __init__(self):
-        self.__total = 0.0  # Total spent on all purchases
-        self.__num_shares = 0  # Total shares bought on all purchases
-        self.__purchases = {}
+        self.__total: float = 0.0  # Total spent on all purchases
+        self.__num_shares: int = 0  # Total shares bought on all purchases
+        self.__purchases: Dict[str, Purchase] = {}
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Deposit):
+            return NotImplemented
         return self.to_dict() == other.to_dict()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return json.dumps(self.to_dict())
 
-    def get_total(self):
+    def get_total(self) -> float:
         return self.__total
 
-    def get_num_shares(self):
+    def get_num_shares(self) -> int:
         return self.__num_shares
 
-    def get_purchases(self):
+    def get_purchases(self) -> Dict[str, Purchase]:
         return self.__purchases
 
-    def get_involved_asset_classes(self):
+    def get_involved_asset_classes(self) -> KeysView:
         return self.get_purchases().keys()
 
-    def to_dict(self):
+    def to_dict(self) -> Dict[str, Any]:
         return {
             "total": self.get_total(),
             "num_shares": self.get_num_shares(),
             "purchases": self.get_purchases(),
         }
 
-    def for_display(self):
-        p_ac = PrettyTable(["Asset Class", "Expenditures"])
-        p_sec = PrettyTable(
+    def for_display(self) -> str:
+        p_ac: PrettyTable = PrettyTable(["Asset Class", "Expenditures"])
+        p_sec: PrettyTable = PrettyTable(
             ["Asset Class", "Security", "Symbol", "Shares", "Price", "Cost"]
         )
         p_ac.title = "Expenditures For Each Asset Class"
         p_sec.title = "Purchases"
-        acs = [
+        acs: List[Tuple[AssetClass, float]] = [
             (ac, self.get_asset_class_expenditures(ac))
             for ac in self.get_involved_asset_classes()
         ]
-        sorted_acs = sorted(acs, key=lambda x: x[1], reverse=True)
+        sorted_acs: List[Tuple[AssetClass, float]] = sorted(
+            acs, key=lambda x: x[1], reverse=True
+        )
         for ac_name, ac_exp in sorted_acs:
             p_ac.add_row([ac_name, dollar_str(ac_exp)])
-            ps = [
+            ps: List[Tuple[Purchase, float]] = [
                 (p, p.get_cost())
                 for p in self.get_purchases_for_asset_class(ac_name)
             ]
-            sorted_ps = sorted(ps, key=lambda x: x[1], reverse=True)
+            sorted_ps: List[Tuple[Purchase, float]] = sorted(
+                ps, key=lambda x: x[1], reverse=True
+            )
             for p, p_cost in sorted_ps:
-                sec = p.get_security()
-                name = sec.get_name()
-                sym = sec.get_symbol()
-                shares = p.get_num_shares()
-                price = dollar_str(sec.get_price())
-                cost = dollar_str(p_cost)
+                sec: Security = p.get_security()
+                name: str = sec.get_name()
+                sym: str = sec.get_symbol()
+                shares: int = p.get_num_shares()
+                price: float = dollar_str(sec.get_price())
+                cost: float = dollar_str(p_cost)
                 p_sec.add_row([ac_name, name, sym, shares, price, cost])
         p_ac.add_row(["Total", dollar_str(self.get_total())])
-        tot_shares = self.get_num_shares()
-        tot_cost = self.get_total()
+        tot_shares: int = self.get_num_shares()
+        tot_cost: float = self.get_total()
         p_sec.add_row(
             ["Total", "-", "-", tot_shares, "-", dollar_str(tot_cost)]
         )
         return "\n{}\n{}".format(p_ac, p_sec)
 
-    def add_purchase(self, asset_class_name, purchase):
+    def add_purchase(self, asset_class_name: str, purchase: Purchase) -> None:
         """
         Adds the given purchase to the list of purchases to be made for the
         given asset class.
@@ -87,14 +96,16 @@ class Deposit:
         self.__total += purchase.get_cost()
         self.__num_shares += purchase.get_num_shares()
 
-    def involves_asset_class(self, asset_class_name):
+    def involves_asset_class(self, asset_class_name: str) -> bool:
         """
         Returns whether this deposit involves purchases made in the given
         asset class.
         """
         return asset_class_name in self.get_involved_asset_classes()
 
-    def get_purchases_for_asset_class(self, asset_class_name):
+    def get_purchases_for_asset_class(
+        self, asset_class_name: str
+    ) -> List[Purchase]:
         """
         Returns the purchases to be made for the given asset class for this
         deposit.
@@ -105,9 +116,11 @@ class Deposit:
             "Deposit does not involve asset class {}.".format(asset_class_name)
         )
 
-    def get_asset_class_expenditures(self, asset_class_name):
+    def get_asset_class_expenditures(self, asset_class_name: str) -> float:
         """
         Returns the total expenditures in the given asset class.
         """
-        ps = self.get_purchases_for_asset_class(asset_class_name)
+        ps: List[Purchase] = self.get_purchases_for_asset_class(
+            asset_class_name
+        )
         return sum(p.get_cost() for p in ps)
