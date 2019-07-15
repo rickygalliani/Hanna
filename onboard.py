59ed88ccaecf8b94e85e8ac8dcbae4abeb680206
Patrick Galliani <pjgalliani@caltech.edu>
2019-07-14 22:22:34 -0700

    Additional note on TODO

--- onboard.py
+++ onboard.py
@@ -9,11 +9,13 @@ import os
 import robin_stocks as r
 
 
-def setup_security() -> Tuple[str, bool]:
+def setup_security(sec_num: int) -> Tuple[str, bool]:
     """
     Collects user input to set up a security".
     """
-    sec_sym: str = input("\t\tEnter the security symbol: ")
+    sec_sym: str = input(
+        "\t\tEnter the symbol of security {}: ".format(sec_num)
+    )
     instrument: Dict[str, Any] = r.get_instruments_by_symbols([sec_sym])[0]
     security_id: str = instrument["id"]
     security_name: str = instrument["name"]
@@ -34,29 +36,29 @@ def setup_security() -> Tuple[str, bool]:
     return (security_id, allow_purchase)
 
 
-def setup_asset_class() -> Dict[str, str]:
+def setup_asset_class(ac_num: int) -> Dict[str, str]:
     """
     Collects user input to set up an asset class.
     """
     asset_class: Dict[str, Any] = {}
 
     # Get name
-    ac_name: str = input("Enter the name of the asset class: ")
+    ac_name: str = input("Enter the name of asset class {}: ".format(ac_num))
     asset_class["name"] = ac_name
 
     # Get target percentage
     ac_target_pct: str = input(
-        "\tEnter target percentage of the asset class (in [0, 1]): "
+        "\tEnter target percentage of the {} (in [0, 1]): ".format(ac_name)
     )
     asset_class["target_percentage"] = float(ac_target_pct)
 
     # Get securities and buy restrictions
     asset_class["securities"] = []
     asset_class["buy_restrctions"] = []
-    add_sec_str: str = input("\tAdd security to {}? [Y/n] ".format(ac_name))
-    add_sec: bool = add_sec_str.lower() == "y" or add_sec_str == ""
+    sec_num: int = 1
+    add_sec: bool = True
     while add_sec:
-        security: Tuple[str, bool] = setup_security()
+        security: Tuple[str, bool] = setup_security(sec_num)
         security_id: str = security[0]
         allow_purchase: bool = security[1]
         asset_class["securities"].append(security_id)
@@ -65,8 +67,8 @@ def setup_asset_class() -> Dict[str, str]:
         add_sec_str: str = input(
             "\tAdd another security to {}? [Y/n] ".format(ac_name)
         )
-        add_sec: bool = add_sec_str.lower() == "y" or add_sec_str == ""
-
+        add_sec = add_sec_str.lower() == "y" or add_sec_str == ""
+        sec_num += 1
     return asset_class
 
 
@@ -79,10 +81,12 @@ if __name__ == "__main__":
 
     config = os.path.join(os.getcwd(), "config", "test_portfolio.json")
 
-    add_asset_class_str: str = input("\nAdd an asset class? [Y/n] ")
-    add_asset_class: bool = (
-        add_asset_class_str.lower() == "y" or add_asset_class_str == ""
-    )
-    if add_asset_class:
-        ac: Dict[str, str] = setup_asset_class()
-        print("ac = {}".format(ac))
+    ac_num: int = 1
+    add_asset_class: bool = True
+    while add_asset_class:
+        ac: Dict[str, str] = setup_asset_class(ac_num)
+        add_asset_class_str: str = input("\nAdd an additional class? [Y/n] ")
+        add_asset_class = (
+            add_asset_class_str.lower() == "y" or add_asset_class_str == ""
+        )
+        ac_num += 1
